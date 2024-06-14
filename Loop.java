7c8414cefda416680f2b0f20c553bcb8bf1c9b15
Galih Juansaputra <102155209+galihjuansaputra@users.noreply.github.com>
2024-06-14 09:16:37 +0700

    Create Loop.java

---
+++ Loop.java
@@ -0,0 +1,17 @@
+class Loop {
+    public static void main(String[] args) {
+        rusak(1000);
+    }
+    
+    public static void rusak(int n) {
+    if (n == 0) {
+        return;
+    }
+
+    for (int i = 1 ; i <= n ; i++) {
+        System.out.println("Hello");
+    }
+
+    rusak((int)n/2);
+    }
+}
